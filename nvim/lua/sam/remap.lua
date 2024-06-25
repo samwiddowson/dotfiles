9967619fbca29844e0aaaf71763ab132a742e3f3
Sam Widdowson <samwid@gmail.com>
2024-06-25 15:21:48 +0100

    Config for a native Linux install

--- nvim/lua/sam/remap.lua
+++ nvim/lua/sam/remap.lua
@@ -2,8 +2,6 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeOpen)
 
-vim.api.nvim_set_keymap('i', 'jj', '<Esc>', {})
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
